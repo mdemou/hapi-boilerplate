abf1b54f3b9e98afe41e732b63e641e64ca939b6
Javier Moure <javiermoure@agentero.com>
2018-05-08 12:28:47 +0200

    Deleted unnecessary config folder

--- server/config/environment/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Development specific configuration
-// ==================================
-module.exports = {
-  ip          : 'localhost',
-  port        : 9000,
-  mongoUrl: 'mongodb://localhost:27017/hermesms',
-};
